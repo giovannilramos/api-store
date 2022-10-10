38d4a6d3561d889a35a4eb57f11b70af69f7d44a
giovanniramos <giovanni.ramos@ti9.com.br>
2022-10-10 16:17:13 -0300

    refactor

--- src/main/java/br/com/quaz/store/controllers/PurchaseController.java
+++ src/main/java/br/com/quaz/store/controllers/PurchaseController.java
@@ -1,6 +1,5 @@
 package br.com.quaz.store.controllers;
 
-import br.com.quaz.store.request.PurchaseRequest;
 import br.com.quaz.store.response.PurchaseResponse;
 import br.com.quaz.store.services.ListPurchaseService;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
