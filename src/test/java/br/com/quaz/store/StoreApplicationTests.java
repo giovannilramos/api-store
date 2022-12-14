26253afb9bce459dbfff7c8da10fcfe3d5e8782b
giovanniramos <giovanni.ramos@ti9.com.br>
2022-12-14 16:51:36 -0300

    added mockito tests

--- src/test/java/br/com/quaz/store/StoreApplicationTests.java
+++ src/test/java/br/com/quaz/store/StoreApplicationTests.java
@@ -3,11 +3,14 @@ package br.com.quaz.store;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 @SpringBootTest
 class StoreApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertNull(null);
 	}
 
 }
