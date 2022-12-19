c63636cb5acd3dd552491a4d6d434b5b9229c1af
giovanniramos <giovanni.ramos@ti9.com.br>
2022-12-19 16:30:54 -0300

    via cep unit test

---
+++ src/test/java/br/com/quaz/store/ViaCepApiServiceTest.java
@@ -0,0 +1,32 @@
+package br.com.quaz.store;
+
+import br.com.quaz.store.exceptions.BadRequestException;
+import br.com.quaz.store.services.ViaCepApiService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class ViaCepApiServiceTest {
+    @Autowired
+    private ViaCepApiService viaCepApiService;
+
+    @Test
+    void shouldReturnDataFromCep() {
+        final var cep = "13052570";
+        final var viaCepJson = viaCepApiService.getAddressByCep(cep);
+        assertEquals("Campinas", viaCepJson.getCity());
+        assertEquals("SP", viaCepJson.getState());
+        assertEquals("Rua Gelsumino Lizardi", viaCepJson.getStreet());
+        assertEquals("Jardim San Diego", viaCepJson.getDistrict());
+    }
+
+    @Test
+    void shouldThrowBadRequestException() {
+        final var cep = "213121";
+        assertThrows(BadRequestException.class, () -> viaCepApiService.getAddressByCep(cep), "Incorrectly cep");
+    }
+}
