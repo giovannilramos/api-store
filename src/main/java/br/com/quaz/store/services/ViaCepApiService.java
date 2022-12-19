7b9257fd8b2236fcc476ceed97c23be633cd1bac
giovanniramos <giovanni.ramos@ti9.com.br>
2022-12-19 16:09:46 -0300

    exception handling

--- src/main/java/br/com/quaz/store/services/ViaCepApiService.java
+++ src/main/java/br/com/quaz/store/services/ViaCepApiService.java
@@ -1,7 +1,8 @@
 package br.com.quaz.store.services;
 
-import br.com.quaz.store.integrations.ViaCepIntegration;
 import br.com.quaz.store.controllers.response.ViaCepResponse;
+import br.com.quaz.store.exceptions.BadRequestException;
+import br.com.quaz.store.integrations.ViaCepIntegration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +15,13 @@ public class ViaCepApiService {
     private final ViaCepIntegration viaCepIntegration;
 
     public ViaCepResponse getAddressByCep(final String cep) {
-        final var jsonAddress = viaCepIntegration.getCep(cep);
-        return convertViaCepDTOToResponse(convertViaCepRequestToDTO(jsonAddress.get("localidade").asText(),
-                jsonAddress.get("uf").asText(), jsonAddress.get("complemento").asText(),
-                jsonAddress.get("logradouro").asText(), jsonAddress.get("bairro").asText()));
+        try {
+            final var jsonAddress = viaCepIntegration.getCep(cep);
+            return convertViaCepDTOToResponse(convertViaCepRequestToDTO(jsonAddress.get("localidade").asText(),
+                    jsonAddress.get("uf").asText(), jsonAddress.get("complemento").asText(),
+                    jsonAddress.get("logradouro").asText(), jsonAddress.get("bairro").asText()));
+        } catch (final Exception e) {
+            throw new BadRequestException("Incorrectly cep");
+        }
     }
 }
