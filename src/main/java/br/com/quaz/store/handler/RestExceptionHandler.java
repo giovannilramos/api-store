4975c5cf8e6b6556e6179799dad9fbaa94da7eba
Giovanni Ramos <giovannilramos55@gmail.com>
2023-03-13 12:44:01 -0300

    refactor: update from ControllerAdvice to RestControllerAdvice

--- src/main/java/br/com/quaz/store/handler/RestExceptionHandler.java
+++ src/main/java/br/com/quaz/store/handler/RestExceptionHandler.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class RestExceptionHandler {
 
     @ExceptionHandler(value = {BaseException.class})
